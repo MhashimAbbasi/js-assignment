16beb5e433ed019c9b0d69d5ce879da420a846dc
mhashimabbasi <mhashimabbasi2311@gmail.com>
2023-02-04 00:19:22 +0500

    practice (alert)
    js 1 smit

---
+++ work 1/app.js
@@ -0,0 +1,27 @@
+
+//Question no 01
+
+alert("Thanks for your input!")
+
+
+
+//Question no 02
+
+alert("Error! Please enter a valid password.")
+
+
+//Question no 03
+
+alert("Welcome to JS Land...\nHappy coding!")
+
+
+//Question no 04
+
+alert("Welcome to JS Land")
+
+
+
+//Question no 05
+
+alert("Hello... I can run JS through my web brower's console")
+
