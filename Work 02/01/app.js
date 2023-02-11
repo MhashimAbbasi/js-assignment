03228b6a7eaa9aa33d3c4249f87c45831263f3dd
mhashimabbasi <mhashimabbasi2311@gmail.com>
2023-02-11 15:42:00 +0500

    via file code upload
    code update

---
+++ Work 02/01/app.js
@@ -0,0 +1,45 @@
+//**********Chap no 2**********
+// Question no 1
+var name = "M. Hashim" 
+alert(name)
+
+
+// Question no 2
+var age = "25 years old" 
+alert(age)
+
+
+// Question no 3
+var process = "Inprocess Web Development & Mobile Hybrid  Application" 
+alert(process)
+
+
+// Question no 4
+var years = "15 years old"
+alert(years)
+
+
+// Question no 5
+
+var pizaa = "PIZZA\nPIZZ\nPIZ\nPI\nP\n"
+alert(pizaa)
+
+ 
+// Question no 6
+var email = "My email address is mhashimabbasi2311@gmail.com"
+alert(email)
+
+
+// Question no 7
+var book = "I am trying to learn from the Book A smarter way to learn Javascript"
+alert(book)
+
+
+
+// Question no 8
+
+document.write = "(<h1> Yah! I can write HTML content through JavaScript </h1>)"
+
+// Question no 9
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" 
+alert(design)
---
+++ Work 02/02/app.js
@@ -0,0 +1,28 @@
+// Assignment # 2 JAVASCRIPT
+    // chapter no 03
+// Question no 1
+
+var etc=("I'M")
+var age=("25")
+var comment=("Year's Old")
+alert(etc + " " + age + " " + comment)
+
+//Question no 2
+var visitor=("14 times ")
+var etc=("You have visited this site.")
+alert(visitor+etc)
+
+//Question no 3
+var dob=("23.Nov <br> ")
+var brithyear=("1996 <br> ")
+document.write("My Date of brith is" + " " + brithyear + " " + "</br> Data type of my declared variable is number.</br></br>")
+
+
+//Question no 4
+var name=("<b> M HASHIM ABBSI</b>")
+var title=("T-sharts")
+var quantity=("50")
+document.write( name +" "+ "ordered" + " " + quantity + " " + title)
+document.write("At <b>abbasistore.com</b>")
+
+
