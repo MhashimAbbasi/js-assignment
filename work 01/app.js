f2d6738ae57a6ee4c088f27e9ee5cbb86fbef108
mhashimabbasi <mhashimabbasi2311@gmail.com>
2023-02-11 15:15:23 +0500

    add work via upload
    class 01

---
+++ work 01/app.js
@@ -0,0 +1,42 @@
+
+//****  Assignment # 1 JAVASCRIPT
+
+
+//****  Chapter no 01    "ALERT"
+
+// Question # 01
+
+var msg = "Hello"
+alert(msg)
+
+// Question # 02
+
+var error = "Error! Please enter a valid password"
+alert(error)
+
+// Question # 03
+
+var msg2 = "Welcome to JS Land...\nHappy Coding!"
+alert(msg2)
+
+// Question # 04
+
+var sequence1 = "Welcome JS Land..."
+alert(sequence1)
+
+var sequence2 = "Happy coding!"
+alert(sequence2)
+
+// Question # 04
+
+var con = "Hello... I can run JS through my web browser's console"
+
+alert(con)
+
+// Question # 05
+
+console.log(con)
+
+// Question # 06
+
+
